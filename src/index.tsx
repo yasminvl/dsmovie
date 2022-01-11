fced3adbccc3a18ab01dc39a2cdb995a2cecfcc2
Yasmin Benicio <79879086+yasminvl@users.noreply.github.com>
2022-01-11 10:30:38 -0300

    Form layout

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from './App';
+
+import './index.css';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
