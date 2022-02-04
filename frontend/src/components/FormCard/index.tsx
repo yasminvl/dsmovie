e593d6be5e0aba5278ceec1af3025a764ab370a4
Yasmin Benicio <79879086+yasminvl@users.noreply.github.com>
2022-02-04 13:13:37 -0300

    css

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -73,11 +73,11 @@ function FormCard({ movieId }: Props) {
                         </select>
                     </div>
                     <div className="dsmovie-form-btn-container">
-                        <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                        <button type="submit" className="btn dsmovie-btn">Salvar</button>
                     </div>
                 </form >
                 <Link to="/">
-                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                    <button className="btn dsmovie-btn mt-3">Cancelar</button>
                 </Link>
             </div >
         </div >
