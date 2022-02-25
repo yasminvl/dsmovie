499870cc6b2299f7d1c850ec0fb7d69bc3210f48
Yasmin Benicio <79879086+yasxxv@users.noreply.github.com>
2022-02-25 10:30:15 -0300

    css

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,10 +8,10 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/yasminvl">
+                    <a href="https://github.com/yasxxv">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/yasminvl</p>
+                            <p className="dsmovie-contact-link">yasxxv</p>
                         </div>
                     </a>
                 </div>
